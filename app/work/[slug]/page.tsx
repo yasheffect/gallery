f8edd5515d1b383fee7a9e9625ed8fe9e6b2f95a
yasheffect <yasheffect@gmail.com>
2026-08-24 16:36:42 +0530

    Restore custom Nike page

--- app/work/[slug]/page.tsx
+++ app/work/[slug]/page.tsx
@@ -2,9 +2,11 @@ import { projectsData } from '../../data/projects';
 import ClientPage from './ClientPage';
 
 export function generateStaticParams() {
-  return projectsData.map((project) => ({
-    slug: project.slug,
-  }));
+  return projectsData
+    .filter(project => project.slug !== 'nike-run-for-life')
+    .map((project) => ({
+      slug: project.slug,
+    }));
 }
 
 export default function WorkDetail({ params }: { params: { slug: string } }) {
